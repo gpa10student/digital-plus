94c91073b5e75e106cdc5230e02d5d361f1f8879
GPA10Student <gpa10student@GPA10Students-MacBook-Air.local>
2021-09-14 17:38:45 +1000

    login page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import Login from './components/Login';
 
 function App(){
-    return(
-        <div className="App">
+    return (
+        <div className = "App">
             <BrowserRouter>
                 <Switch>
                     <Route><Login/></Route>
@@ -12,6 +12,9 @@ function App(){
             </BrowserRouter>
         </div>
     )
+        
+
+
 }
 
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,36 +1,38 @@
 import React from 'react';
-import "./Login.css";
+import './Login.css';
 
-const Login = () =>{
-    return(
-        <div className="login-container">
-            <div className="login-wrapper">
-                <input id='tab1' type='radio' className='sign-in' name='tab' checked/><label for = 'tab1' className='tab'>Sign in</label>
-                <input id='tab2' type='radio' className='sign-up' name='tab'/><label for = 'tab2' className='tab'>Sign up</label>
-
-                <div className='form-container'>
-                    <div className='signin'>
-                        <form className='signin-form'>
-                            <input type='text' placeholder="Username"/>
-                            <input type='password' placeholder='Password'/>
-                            <button type='submit'>Sign in</button>
-                        </form>
-                        <div className='link'>
-                            <a href='#'>Forgot password?</a>
+const Login = () => {
+    return (
+        <div>
+            <div className="login-container">
+                <div className="login-wrapper">
+                    <input id="tab1" type='radio' name='tab' class="sign-in" checked/><label for="tab1" class="tab">Sign In</label>
+                    <input id="tab2" type='radio' name='tab' class="sign-up" /><label for="tab2" class="tab">Sign Up</label>
+                    <div className="form-container">
+                        <div className="signin">
+                            <form className="signin-form">
+                                <input id="username" type="text" class="input" placeholder="Username"/>
+                                <input id="password" type ="password" class="input" data-type="password" placeholder="Password"/>
+                                <button type="submit" ><b>Sign in</b></button>
+                            </form>
+                            <div className="link">
+                                <a>Forgot password?</a>
+                            </div>
+                        </div>
+                        <div className="signup">
+                            <form className="signup-form">
+                                <input id="username" type="text" class="input" placeholder="Username"/>
+                                <input id="password" type ="password" class="input" data-type="password" placeholder="Password"/>
+                                <input id="password" type ="password" class="input" data-type="password" placeholder="Confirm your Password"/>
+                                <button type="submit" ><b>Sign up</b></button>
+                            </form>
                         </div>
                     </div>
-
-                    <div className='signup'>
-                        <form className='signup-form'>
-                            <input type='text' placeholder="Username"/>
-                            <input type='password' placeholder='Password'/>
-                            <input type='password' placeholder='Confirm your password'/>
-                            <button type='submit'>Sign up</button>
-                        </form>
-                    </div>
+                    
                 </div>
             </div>
         </div>
+        
     )
 }
 
